dedd3cdac1db628cd4cdf190530ecec654ddbb8d
Gustavo Moreno <gustavo@laenredadera.net>
2023-10-09 19:01:01 +0200

    added custom bloc provider

--- lib/pages/counter/counter_page.dart
+++ lib/pages/counter/counter_page.dart
@@ -1,43 +1,32 @@
 import 'package:bloc_concept/pages/bloc/counter_bloc.dart';
 import 'package:bloc_concept/pages/bloc/counter_events.dart';
-import 'package:bloc_concept/pages/counter/counter_cubit.dart';
+import 'package:bloc_concept/state_managment/bloc_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class CounterPage extends StatefulWidget {
+class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
 
   @override
-  State<CounterPage> createState() => _CounterPageState();
+  Widget build(BuildContext context){
+    return BlocProvider<CounterBloc>(
+        create: (_)=> CounterBloc(0),
+        child: Builder(builder: ( context) {
+            final CounterBloc _bloc = context.watch();
+            return Scaffold(
+              body: Center(
+                  child:Text(
+                    '${_bloc.state}',
+                    style: const TextStyle(fontSize: 25),
+                  ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  _bloc.add(Incremented(10));
+                },
+              ),
+            );
+          }
+        ));
 }
-
-class _CounterPageState extends State<CounterPage> {
-
-  final _bloc = CounterBloc(90);
-
-  @override
-  void dispose() {
-    _bloc.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: StreamBuilder<int>(
-            stream: _bloc.stream,
-            initialData: _bloc.state,
-            builder: (_, snapshot) => Text(
-              '${_bloc.state}',
-              style: const TextStyle(fontSize: 25),
-            ),
-          )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _bloc.add(Incremented(10));
-          },
-      ),
-    );
-  }
 }
